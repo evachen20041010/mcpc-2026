0f6cf5d2151a383d3e97d48c758a0c43eff621be
EvaChen <a0937510149@gmail.com>
2026-09-10 15:30:36 +0800

    更新競賽海報

--- src/data/downloadData.ts
+++ src/data/downloadData.ts
@@ -17,8 +17,8 @@ export const downloadItems: DownloadItem[] = [
     desc: "可下載對外宣傳使用。",
     fileType: "PNG",
     updatedAt: "",
-    href: "/downloads/poster_r6.png",
-    previewSrc: "/downloads/poster_r6.png",
+    href: "/downloads/poster_r7.png",
+    previewSrc: "/downloads/poster_r7.png",
     isAvailable: true,
   },
   {
